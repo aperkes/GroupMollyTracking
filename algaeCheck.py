b0df4d9b95e35ef6fa62d8acf34ff9a75f28b5a7
aperkes <perkes.ammon@gmail.com>
2024-10-30 17:30:30 -0700

    it works now! remember opencv is (y,x)

--- algaeCheck.py
+++ algaeCheck.py
@@ -5,7 +5,7 @@ from scipy import stats
 import numpy as np
 
 from matplotlib import pyplot as plt
-import sys
+import sys,os
 
 from processTracks import get_tracks,clean_track
 
@@ -14,9 +14,12 @@ from processTracks import get_tracks,clean_track
 ### Quantifying algae:
 # Take the mode of the background for every video (ugh) 
 # Bin the background, excluding the edges 
-def get_algae_locs(vid_file,mask_file = None,down_ratio=0.1,n_frames=100):
+def get_algae_locs(vid_file,mask = None,down_ratio=0.1,n_frames=100):
     cap = cv2.VideoCapture(vid_file)
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    if total_frames < n_frames:
+        #import pdb;pdb.set_trace()
+        raise Exception("Vid file too short")
     n_skip = total_frames / n_frames
 
     width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -48,11 +51,13 @@ def get_algae_locs(vid_file,mask_file = None,down_ratio=0.1,n_frames=100):
 
     br,_ = stats.mode(small_frames,axis=0)
 
+    br_masked = np.array(br)
+    #import pdb;pdb.set_trace()
     if mask is not None:
-        print('masking...')
-        br[~mask] = np.nan
+        #print('masking...')
+        br_masked[~mask] = np.nan
 
-    algae_flat = br.flatten()
+    algae_flat = br_masked.T.flatten() ## need to get it into xy coord space
     algae_flat = algae_flat[~np.isnan(algae_flat)]
     return algae_flat,br
 
@@ -83,25 +88,68 @@ def get_mask(mask_path='./br-mask.png',down_ratio=0.1):
 
     return mask
 
+## opencv uses [row,column] (i.e., y,x). I forget this once per year. 
+def get_pixIDs(w=800,h=800,down_ratio=0.1):
+    if w <= 1000:
+        y_scale = int(1000 * down_ratio)
+    else:
+        y_scale = int(w * down_ratio)
+    
+    w = int(w * down_ratio)
+    h = int(h * down_ratio)
+
+    id_array = np.empty([w,h])
+    xy_array = np.empty([w,h,2])
+    yx_array = np.empty([w,h,2])
+
+    for x in np.arange(w): ## There's almost certainly a more clever way to do this
+        for y in np.arange(h):
+            id_array[x,y] = x + y*y_scale
+            xy_array[x,y] = (x,y)
+            yx_array[x,y] = (y,x)
+    return id_array,xy_array,yx_array
+
 if __name__ == '__main__':
     
-    vid_path = sys.argv[1]
-    csv_path = sys.argv[2]
+## This could be arg parsed...
+    try:
+        vid_path = sys.argv[1]
+        csv_path = sys.argv[2]
+        out_csv = sys.argv[3]
+    except:
+        print("needs 3 arguments, vid path, csv path, and out_csv")
     mask = get_mask()
+    maskT = mask.T ## opencv imgs are in y,x!!!
+    id_array,xy_array,yx_array = get_pixIDs()
+    flat_ids = id_array[maskT != 0].flatten()
+    flat_xys = xy_array[maskT != 0].reshape([-1,2])
+    #mask=None
+
     algae_flat,br = get_algae_locs(vid_path,mask)
     flat_counts,counts = get_fish_locs(csv_path,mask)
-    print(stats.pearsonr(algae_flat,flat_counts))
+    
+    basename = os.path.basename(vid_path)
+    basename = basename.replace('.mp4','')
+    pi,day = basename.split('_')
 
-    fig,ax=plt.subplots()
+    if True:
+        with open(out_csv,'a') as f:
+            for i in range(len(algae_flat)):
+                x,y = flat_xys[i]
+                line_list = [flat_ids[i],x,y,pi,day,algae_flat[i],flat_counts[i]]
+                line = ','.join([str(v) for v in line_list]) + '\n'
+                #print(line)
+                f.write(line)
+    if True:
+        print(stats.pearsonr(algae_flat,flat_counts))
+        fig,ax=plt.subplots()
 
-    ax.imshow(br,cmap='gray')
+        ax.imshow(br.T,cmap='gray')
+        ax.imshow(counts,alpha=0.5,vmax=100)
+        #cv2.imwrite('./br.png',br)
+        plt.savefig('./imgs3/' + basename + '.png',bbox_inches='tight',dpi=300)
 
-#cv2.imwrite('./br.png',br)
-    plt.show()
-
-
-    import pdb;pdb.set_trace()
-
+    print('done!')
 ## At some point I need to move to R probably
 
 # Binwise correlation of algae to space use
--- plotHeatmap.py
+++ plotHeatmap.py
@@ -35,43 +35,48 @@ def norm_array(arr,vmin,vmax):
   
 if __name__ == "__main__":
 
+    csv_file = sys.argv[1]
+    TEXT = False   
+    #csv_file = sys.argv[1]
+    if len(sys.argv) > 2:
+        vid_file = sys.argv[2]
+    else:
+        vid_file = None
+    #csv_path = './groupCSVs/' + csv_file
+    csv_path = csv_file
 
-    for csv_file in os.listdir('./groupCSVs'):
-        TEXT = False   
-        #csv_file = sys.argv[1]
-        if len(sys.argv) > 2:
-            vid_file = sys.argv[2]
-        else:
-            vid_file = None
-        csv_path = './groupCSVs/' + csv_file
-        track_array,track_polar,(n_frames,n_fish,fishIDs) = get_tracks(csv_path)
-        clean_array = clean_track(track_array)
-        track_array = clean_array
+    track_array,track_polar,(n_frames,n_fish,fishIDs) = get_tracks(csv_path)
+    clean_array = clean_track(track_array)
+    #track_array = clean_array
 ## Calculate lots of stat arrays for plotting
 
-        print(track_array.shape)
-        flat_tracks = np.reshape(track_array,[-1,2])
-        
-        print(flat_tracks.shape)
-        flat_tracks = flat_tracks[~np.isnan(flat_tracks[:,0])]
-        print(flat_tracks.shape)
-        print(np.min(flat_tracks),np.max(flat_tracks))
+    print(track_array.shape)
+    flat_tracks = np.reshape(track_array,[-1,2])
+    
+    print(flat_tracks.shape)
+    flat_tracks = flat_tracks[~np.isnan(flat_tracks[:,0])]
+    print(flat_tracks.shape)
+    print(np.min(flat_tracks),np.max(flat_tracks))
 
-        x_edges = np.arange(0,801,10)
-        y_edges = x_edges
-        #foo = np.histogram2d(flat_tracks[:,0],flat_tracks[:,1],bins=80)
-        foo = np.histogram2d(flat_tracks[:,0],flat_tracks[:,1],bins=x_edges)
-        fig,ax=plt.subplots()
-        if vid_file is not None:
-            cap = cv2.VideoCapture(vid_file)
-            cap.set(cv2.CAP_PROP_POS_FRAMES,3600) ## skip initial darkness
-            res,frame = cap.read()
-            ax.imshow(frame)
-            alpha = 0.3
-        else:
-            alpha = None
-        ax.imshow(foo[0],vmin=0,vmax=100,alpha=alpha,cmap='viridis',extent=[0,800,0,800])
+    x_edges = np.arange(0,801,10)
+    y_edges = x_edges
+    #foo = np.histogram2d(flat_tracks[:,0],flat_tracks[:,1],bins=80)
+    foo = np.histogram2d(flat_tracks[:,0],flat_tracks[:,1],bins=x_edges)
+    import pdb;pdb.set_trace()
+    fig,ax=plt.subplots()
+    if vid_file is not None:
+        cap = cv2.VideoCapture(vid_file)
+        cap.set(cv2.CAP_PROP_POS_FRAMES,3600) ## skip initial darkness
+        res,frame = cap.read()
+        ax.imshow(frame)
+        alpha = 0.5
+    else:
+        alpha = None
+    bar = np.flipud(foo[0])
+    #bar = np.fliplr(bar)
+    #ax.imshow(np.flipud(foo[0]),vmin=0,vmax=250,alpha=alpha,cmap='viridis',extent=[0,800,0,800])
+    #ax.imshow(bar,vmin=0,vmax=250,alpha=alpha,cmap='viridis',extent=[0,799,0,799])
+    ax.imshow(bar,vmin=0,vmax=250,alpha=alpha,cmap='viridis')
+    plt.show()
 
-        fig.savefig('./imgs/' + csv_file.replace('.csv','png'),dpi=300)
-        #plt.show()
-        #import pdb;pdb.set_trace()
+    fig.savefig('./imgs2/' + os.path.basename(csv_file).replace('.csv','png'),dpi=300)
