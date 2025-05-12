2dc10d16d2a5ffa1301b344ec784753891c97b20
Ammon Perkes <perkes.ammon@gmail.com>
2025-05-12 07:44:02 -0700

    added a slider

--- plotHeatmap.py
+++ plotHeatmap.py
@@ -7,6 +7,7 @@ import cv2
 from matplotlib import pyplot as plt
 from matplotlib import animation
 from matplotlib.collections import LineCollection
+from matplotlib.widgets import Slider
 
 from processTracks import get_stats,get_meta,get_tracks,get_distance,clean_track
 
@@ -46,11 +47,53 @@ if __name__ == "__main__":
     csv_path = csv_file
 
     track_array,track_polar,(n_frames,n_fish,fishIDs) = get_tracks(csv_path)
+    #import pdb;pdb.set_trace()
     clean_array = clean_track(track_array)
     #track_array = clean_array
 ## Calculate lots of stat arrays for plotting
 
     print(track_array.shape)
+
+    n_tracks = track_array.shape[1]
+    window_size = 20
+    half_win = window_size // 2 
+    n_frames = track_array.shape[0]
+
+    fig,ax = plt.subplots()
+    lines = []
+    points = []
+    from matplotlib import colormaps
+
+    cmap = colormaps.get_cmap('tab10')
+    cors = [cmap(i) for i in range(n_tracks)] 
+    for i in range(n_tracks):
+
+        line, = ax.plot([],[],label=f'Trajectory {i+1}',c=cors[i])
+        point, = ax.plot([],[],'o',markersize=8,c=cors[i])
+        points.append(point)
+        lines.append(line)
+
+    ax.set_xlim([0,800])
+    ax.set_ylim([0,800])
+    
+    ax_slider = fig.add_axes([0.25,0.1,0.65,0.03])
+    slider = Slider(ax=ax_slider,label='Frame Index',valmin=0,valmax = n_frames,valinit=100,valstep=1)
+
+    def update(frame_index):
+        start = max(0,frame_index - half_win)
+        end = min(n_frames,frame_index + half_win+1)
+
+        for i,line in enumerate(lines):
+            line.set_data(clean_array[start:end,i,0],clean_array[start:end,i,1])
+            points[i].set_data(clean_array[end-1,i,0],clean_array[end-1,i,1])
+
+        fig.canvas.draw_idle()
+        return lines
+
+    slider.on_changed(update)
+    plt.axis('off')
+    plt.show()
+
     flat_tracks = np.reshape(track_array,[-1,2])
     
     print(flat_tracks.shape)
@@ -62,7 +105,6 @@ if __name__ == "__main__":
     y_edges = x_edges
     #foo = np.histogram2d(flat_tracks[:,0],flat_tracks[:,1],bins=80)
     foo = np.histogram2d(flat_tracks[:,0],flat_tracks[:,1],bins=x_edges)
-    import pdb;pdb.set_trace()
     fig,ax=plt.subplots()
     if vid_file is not None:
         cap = cv2.VideoCapture(vid_file)
@@ -79,4 +121,4 @@ if __name__ == "__main__":
     ax.imshow(bar,vmin=0,vmax=250,alpha=alpha,cmap='viridis')
     plt.show()
 
-    fig.savefig('./imgs2/' + os.path.basename(csv_file).replace('.csv','png'),dpi=300)
+    fig.savefig('./figs/' + os.path.basename(csv_file).replace('.csv','png'),dpi=300)
